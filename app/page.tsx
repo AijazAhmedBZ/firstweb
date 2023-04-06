a461fe9cab9bf7f8d2d3e673ac2ce60c953a1e71
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-04-06 18:14:12 +0500

    Updated on 06-Apr-23

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div className="bg-gray py-10 shadow-2xl">
+      <div className="mt-2">
         <Navbar />
       </div>
     </>
