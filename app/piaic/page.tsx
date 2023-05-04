94df4d93c9ed841f4f26d23a41d16d8d62baadf4
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-05-04 06:59:50 +0500

    updated on 04-May-23

--- app/piaic/page.tsx
+++ app/piaic/page.tsx
@@ -6,85 +6,83 @@ import Piaic_buttons from "../components/piaic_buttons";
 const Piaic = () => {
   return (
     <div>
-      <div className="mt-2">
+      <section className="mt-2">
         <Navbar />
-      </div>
-      <header className="mt-7 flex justify-around max-w-screen-xl">
+      </section>
+      <header className="mt-7 flex justify-between max-w-screen-xl mx-auto items-center py-6 px-2 sticky top-0 blur-0 flex-col md:flex-row">
         <Image
           src="./PIAICLogo.svg"
           alt="Logo"
           width="100"
           height="100"
         ></Image>
-        <ul className="mt-7 flex justify-end mr-10">
-          <li className="text-teal-950 text-center font-semibold rounded-full px-5 m-1 py-2 shadow- hover:bg-teal-950 hover:text-white">
-            <Link href="hiw">How it works</Link>
+        <ul className="space-x-8 space-y-8 flex font-semibold items-between flex-col md:flex-row">
+          <br />
+          <li>
+            <Link href={"hiw"}>How it works</Link>
           </li>
-          <li className="text-teal-950 text-center font-semibold rounded-full px-5 m-1 py-2 shadow- hover:bg-teal-950 hover:text-white">
+          <li>
             <Link href="https://portal.piaic.org/signup">Apply</Link>
           </li>
-          <li className="text-teal-950 text-center font-semibold rounded-full px-5 m-1 py-2 shadow- hover:bg-teal-950 hover:text-white">
-            Available Programs
+          <li>Available Programs</li>
+          <li>
+            <Link href={"about"}>About</Link>
           </li>
-          <li className="text-teal-950 text-center font-semibold rounded-full px-5 m-1 py-2 shadow- hover:bg-teal-950 hover:text-white">
-            About
-          </li>
-          <li className="text-teal-950 text-center font-semibold rounded-full px-5 m-1 py-2 shadow- hover:bg-teal-950 hover:text-white">
-            WIT
+          <li>
+            <Link href={"wit"}>WIT</Link>
           </li>
         </ul>
       </header>
-      <div className="mt-5 ml-20 mr-10 flex justify-between">
-        <div className="p-7 mr-10 ml-10 mt-5 bg-gray-300 rounded-lg">
+      <section className="max-w-screen-xl mx-auto m-10 flex flex-col md:flex-row items-center">
+        <div className="p-7 mt-5 mr-10 bg-gray-300 rounded-lg">
           <span>
-            <h1 className="flex flex-wrap text-6xl font-bold text-teal-950">
+            <h1 className="text-3xl md:text-5xl font-bold text-teal-950">
               Presidential Initiative for Artificial Intelligence & Computing
               (PIAIC)
             </h1>
             <br />
             <br />
-            <br />
-            <h1 className="flex flex-wrap text-xl">
+            <h1 className="text-xl">
               The mission of PIAIC is to reshape Pakistan by revolutionizing
               education, research, and business by adopting latest, cutting-edge
               technologies. Experts are calling this the 4th industrial
               revolution. We want Pakistan to become a global hub for AI, data
               science, cloud native computing, edge computing, blockchain,
-              <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-teal-950 relative inline-block">
-                <span className="relative text-white">augmented reality</span>
+              <span className="before:absolute before:-inset-1 before:-skew-y-3 before:bg-teal-950 relative inline-block">
+                <span className="relative text-white">, augmented reality</span>
               </span>
               , and internet of things.
             </h1>
           </span>
         </div>
-        <Image src="/aa.png" alt="President" width="300" height="300"></Image>
-      </div>
-      <div className="p-2 mr-10 ml-10 mt-5 bg-gray-300 rounded-full text-2xl text-center hover:bg-teal-700 hover:text-white hover:py-2">
-        Available Programs
-      </div>
-      <div>
-        <div className="p-2 bg-gray-300 text-center container gap-8 grid grid-cols-4 mt-5 mr-10 ml-10 justify-between rounded-full">
-          <div className="hover:bg-teal-700 hover:text-white hover:py-2 rounded-full text-center">
+        <Image src="/aa.png" alt="President" width="200" height="200"></Image>
+      </section>
+      <section className="max-w-screen-xl mx-auto m-10 flex flex-col md:flex-row items-center">
+        <div className="p-2 mr-10 ml-10 mt-5 bg-gray-300 rounded-full text-2xl text-center hover:bg-teal-700 hover:text-white hover:py-2">
+          Available Programs
+        </div>
+        <ul className="grid grid-cols-1 max-w-screen-xl mx-auto m-10 md:grid-cols-4 items-center">
+          <li>
             <Link href="ai">Artificial Intelligence</Link>
-          </div>
-          <div className="hover:bg-teal-700 hover:text-white hover:py-2 rounded-full text-center">
+          </li>
+          <li>
             <Link href="cnc">
               Cloud Native and Mobile Web Computing Specialist
             </Link>
-          </div>
-          <div className="hover:bg-teal-700 hover:text-white hover:py-2 rounded-full text-center">
+          </li>
+          <li>
             <Link href="bc">BLOCKCHAIN Specialist</Link>
-          </div>
-          <div className="hover:bg-teal-700 hover:text-white hover:py-2 rounded-full text-center">
+          </li>
+          <li>
             <Link href="iot">Internet of Things and AI Specialist</Link>
-          </div>
+          </li>
+        </ul>
+        <div className="justify-center">
+          <h1 className="p-7 justify-centert rounded-full px-5 m-1 py-2 hover:text-teal-700 text-center font-semibold">
+            <Link href="https://portal.piaic.org/signup">Apply</Link>
+          </h1>
         </div>
-      </div>
-      <div className="justify-center">
-        <h1 className="p-7 justify-centert rounded-full px-5 m-1 py-2 hover:text-teal-700 text-center font-semibold">
-          <Link href="https://portal.piaic.org/signup">Apply</Link>
-        </h1>
-      </div>
+      </section>
     </div>
   );
 };
---
+++ app/wit/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar";
+const Wit = () => {
+  return (
+    <div>
+      <div className="mt-2">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Wit;
